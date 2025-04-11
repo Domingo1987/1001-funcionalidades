fa45d8e12d1e569b99c553ab5291653b1b177682
Domingo1987 <domingoperez1987@gmail.com>
2025-04-11 12:54:41 -0300

    6.1.7

--- includes/hooks/interfaz.php
+++ includes/hooks/interfaz.php
@@ -47,10 +47,16 @@ add_shortcode('redireccionar', 'redireccionar_si_no_logueado');
 add_action('wp_footer', 'codigos_footer');
 function codigos_footer() {
     ?>
-    <article data-theme="pico" class="footer-card-pico">
-        <h2 class="footer-numero"><?php echo do_shortcode('[cantidad_codigo]'); ?></h2>
-        <p class="footer-descripcion">CÓDIGOS</p>
-    </article>
+    <div style="display: flex; flex-direction: column; align-items: flex-start; gap: 1rem; margin-left: 1rem;">
+        <article class="footer-card-pico">
+            <h2 class="footer-numero"><?php echo do_shortcode('[cantidad_codigo]'); ?></h2>
+            <p class="footer-descripcion">CÓDIGOS</p>
+        </article>
+        <article class="footer-card-pico">
+            <h2 class="footer-numero"><?php echo do_shortcode('[cantidad_comentarios]'); ?></h2>
+            <p class="footer-descripcion">SOLUCIONES</p>
+        </article>
+    </div>
     <?php
 }
 
