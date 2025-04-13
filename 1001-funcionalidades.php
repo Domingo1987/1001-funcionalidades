0aa4ef902c0506615b75efcc693e8a93609a573e
Domingo1987 <domingoperez1987@gmail.com>
2025-04-13 17:59:08 -0300

    6.9.13

--- 1001-funcionalidades.php
+++ 1001-funcionalidades.php
@@ -3,7 +3,7 @@
  * Plugin Name:       1001 Funcionalidades
  * Plugin URI:        https://1001problemas.com/
  * Description:       Conjunto de  funcionalidades personalizadas para el sitio 1001problemas.com: shortcodes, estadísticas de usuario, scripts interactivos, mejoras visuales y más.
- * Version:           6.9.11
+ * Version:           6.9.13
  * Requires at least: 5.5
  * Requires PHP:      7.4
  * Author:            Domingo Pérez
