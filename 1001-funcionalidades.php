568146988fc60d2c764dfb0251a2cf6607d02afa
Domingo1987 <domingoperez1987@gmail.com>
2025-04-30 14:52:54 -0300

    7.8.0

--- 1001-funcionalidades.php
+++ 1001-funcionalidades.php
@@ -3,7 +3,7 @@
  * Plugin Name:       1001 Funcionalidades
  * Plugin URI:        https://1001problemas.com/
  * Description:       Conjunto de  funcionalidades personalizadas para el sitio 1001problemas.com: shortcodes, estadísticas de usuario, scripts interactivos, mejoras visuales y más.
- * Version:           7.7.0
+ * Version:           7.8.0
  * Requires at least: 5.5
  * Requires PHP:      7.4
  * Author:            Domingo Pérez
@@ -13,6 +13,8 @@
  * Text Domain:       cienuno
  * Domain Path:       /languages
  */
+
+// TODO: ver tema pico dark para que funcione siempre en light
 
 defined('ABSPATH') or die('No script kiddies please!');
 
